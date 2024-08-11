7c92a5c73dd992b5b282de9f1ca3cabfd64eb60a
Grant Orndorff <grant.orndorff@canonical.com>
2024-08-10 21:37:51 -0400

    hd44780 working

--- app/src/main.c
+++ app/src/main.c
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <string.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/auxdisplay.h>
 #include <zephyr/logging/log.h>
 
 #include <app/drivers/blink.h>
@@ -18,6 +21,29 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
+    int rc;
+	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(auxdisplay_0));
+	uint8_t data[64];
+
+	if (!device_is_ready(dev)) {
+		LOG_ERR("Auxdisplay device is not ready.");
+		return 0;
+	}
+
+	rc = auxdisplay_cursor_set_enabled(dev, false);
+
+	if (rc != 0) {
+		LOG_ERR("Failed to enable cursor: %d", rc);
+	}
+
+	snprintk(data, sizeof(data), "Hello world from %s", CONFIG_BOARD);
+	rc = auxdisplay_write(dev, data, strlen(data));
+
+	if (rc != 0) {
+		LOG_ERR("Failed to write data: %d", rc);
+	}
+	return 0;
+
 	int ret;
 	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
 	const struct device *sensor, *blink;
@@ -77,4 +103,3 @@ int main(void)
 
 	return 0;
 }
-

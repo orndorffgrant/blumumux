db1ee41aca7b20565fd55d7e2f01b3ee9db8864d
Grant Orndorff <grant.orndorff@canonical.com>
2024-08-13 23:05:27 -0400

    basic msgq

--- app/src/main.c
+++ app/src/main.c
@@ -1,8 +1,4 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
+#include <stdint.h>
 #include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
@@ -18,59 +14,68 @@
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 const struct device *const knob_dev = DEVICE_DT_GET(DT_NODELABEL(knob_0));
-K_MSGQ_DEFINE(mouse_msgq, MOUSE_REPORT_COUNT, 2, 1);
+K_MSGQ_DEFINE(knob_msgq, 4, 2, 1);
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+#define NUM_CHANNELS 8
 
 static void knob_cb(struct input_event *evt)
 {
 	printk("Knob input detected, code: %d, value: %d\n", evt->code, evt->value);
 
-	/*
-	if (k_msgq_put(&mouse_msgq, tmp, K_NO_WAIT) != 0) {
+	if (k_msgq_put(&knob_msgq, &evt->value, K_NO_WAIT) != 0) {
 		LOG_ERR("Failed to put new input event");
 	}
-	*/
 }
 
 INPUT_CALLBACK_DEFINE(knob_dev, knob_cb);
 
+void display_line(const struct device *dev, const char *line, const int line_no)
+{
+    // TODO probably need a mutex here
+    // OR put the display in a separate thread
+    int rc;
+
+    rc = auxdisplay_cursor_position_set(dev, AUXDISPLAY_POSITION_ABSOLUTE, 0, line_no);
+    if (rc != 0) {
+        LOG_ERR("Failed to move cursor: %d", rc);
+    }
+
+    rc = auxdisplay_write(dev, line, 16);
+    if (rc != 0) {
+        LOG_ERR("Failed to write data: %d", rc);
+    }
+}
 int main(void)
 {
     int rc;
-	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(auxdisplay_0));
-	uint8_t data[64];
+	const struct device *const display_dev = DEVICE_DT_GET(DT_NODELABEL(auxdisplay_0));
+	uint8_t data[17];
 
-	if (!device_is_ready(dev)) {
+	if (!device_is_ready(display_dev)) {
 		LOG_ERR("Auxdisplay device is not ready.");
 		return 0;
 	}
 
-	rc = auxdisplay_cursor_set_enabled(dev, false);
+	rc = auxdisplay_cursor_set_enabled(display_dev, false);
 
-	if (rc != 0) {
-		LOG_ERR("Failed to enable cursor: %d", rc);
+	int curr_channel = 0;
+	uint32_t direction = 0;
+
+	snprintk(data, 17, "Source Device %d ", curr_channel + 1);
+	display_line(display_dev, data, 0);
+
+	while (true) {
+		k_msgq_get(&knob_msgq, &direction, K_FOREVER);
+		curr_channel = (curr_channel + direction) % NUM_CHANNELS;
+
+		snprintk(data, 17, "Source Device %d ", curr_channel + 1);
+		display_line(display_dev, data, 0);
+
+		// TODO
+		// cancel existing utility thread if running
+		// start utility thread with 500ms delay
+		// utility thread will cancel existing ble audio thread if running
+		// then start new ble audio thread on new channel
 	}
-
-	snprintk(data, sizeof(data), "Hi Liana how areyou doing?");
-	rc = auxdisplay_write(dev, data, strlen(data));
-
-	if (rc != 0) {
-		LOG_ERR("Failed to write data: %d", rc);
-	}
-
-	int curr_channel = 1;
-
-
-	while (1) {
-        rc = auxdisplay_channel_select(dev, channel);
-        if (rc != 0) {
-            LOG_ERR("Failed to select channel: %d", rc);
-        }
-        channel = (channel + 1) % 2;
-        k_sleep(K_MSEC(1000));
-    }
-
 	return 0;
 }
